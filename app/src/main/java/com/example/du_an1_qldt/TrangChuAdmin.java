767e7b9a7f5b31758362942cab4199a304178aa4
TienVinh03 <vinhtbtb01@gmail.com>
2024-03-18 21:33:05 +0700

    test 2

---
+++ app/src/main/java/com/example/du_an1_qldt/TrangChuAdmin.java
@@ -0,0 +1,14 @@
+package com.example.du_an1_qldt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrangChuAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chu_admin);
+    }
+}
