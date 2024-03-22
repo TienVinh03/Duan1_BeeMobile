560b86e75168c185966bddbcec52df72ea98f270
hainx.dev.it <hainx.dev@gmail.com>
2024-03-22 10:24:29 +0700

    change file database in main package

--- app/src/main/java/com/example/du_an1_qldt/database/dbHelper.java
+++ app/src/main/java/com/example/du_an1_qldt/database/dbHelper.java
@@ -1,4 +1,4 @@
-package database;
+package com.example.du_an1_qldt.database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
--- app/src/main/java/com/example/du_an1_qldt/model/phone.java
+++ app/src/main/java/com/example/du_an1_qldt/model/phone.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.du_an1_qldt.model;
 
 public class phone {
     private int id;
