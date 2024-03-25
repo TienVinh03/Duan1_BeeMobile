971824f6390a6239994a96df8a790af8c066f814
hainx.dev.it <hainx.dev@gmail.com>
2024-03-25 21:20:18 +0700

    edit layout

---
+++ app/src/main/java/com/example/du_an1_qldt/activity_frag_don_mua.java
@@ -0,0 +1,25 @@
+package com.example.du_an1_qldt;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class activity_frag_don_mua extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_frag_don_mua,container,false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
