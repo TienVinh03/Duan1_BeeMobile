482574ba4bb1bd9b856b5d2b99447df817fc312c
phuongle2004 <lphuong241124@gmail.com>
2024-03-26 15:06:57 +0700

    list danh sách sp

---
+++ app/src/main/java/com/example/du_an1_qldt/Adapter/DSSP_Adapter.java
@@ -0,0 +1,90 @@
+package com.example.du_an1_qldt.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.du_an1_qldt.DAO.SanPhamDAO;
+import com.example.du_an1_qldt.R;
+import com.example.du_an1_qldt.TrangChuNguoiDung;
+import com.example.du_an1_qldt.model.phone;
+
+import java.util.ArrayList;
+
+public class DSSP_Adapter extends RecyclerView.Adapter< DSSP_Adapter.ViewHolder_DSSP   > {
+
+
+    Context context;
+    TrangChuNguoiDung tcnd;
+    ArrayList<phone> listSP;
+    SanPhamDAO dsDao;
+
+    public DSSP_Adapter(Context context, ArrayList<phone> listSP) {
+        this.context = context;
+        this.listSP = listSP;
+        dsDao= new SanPhamDAO(context);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder_DSSP onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater= ((Activity)context).getLayoutInflater();
+        View v = inflater.inflate(R.layout.item_dssp,parent,false);
+        ViewHolder_DSSP holder= new ViewHolder_DSSP(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder_DSSP holder, int position) {
+
+
+        phone dt = listSP.get(position);
+        holder.tv_tenSP1.setText( String.valueOf(dt.getName()) );
+        holder.tv_giaSPham.setText( String.valueOf( dt.getGia()));
+        holder.tv_TrangthaiSP1.setText( String.valueOf(dt.getStatus() ));
+
+
+        phone phoneDTO = listSP.get(position);
+
+        if (phoneDTO.getStatus()==1){
+            holder.tv_TrangthaiSP1.setText("Còn hàng");
+        }else {
+            holder.tv_TrangthaiSP1.setText("Hết hàng");
+        }
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return listSP.size();
+    }
+
+    public class ViewHolder_DSSP extends RecyclerView.ViewHolder{
+        TextView tv_tenSP1,tv_giaSPham,tv_TrangthaiSP1;
+        ImageView anh1, btnThemvaoGiohang,btnMuaNgay;
+
+        public ViewHolder_DSSP(@NonNull View itemView) {
+            super(itemView);
+
+            tv_tenSP1 = itemView.findViewById(R.id.tv_tenSP1);
+            tv_giaSPham = itemView.findViewById(R.id.tv_giaSPham);
+            tv_TrangthaiSP1 = itemView.findViewById(R.id.tv_TrangthaiSP1);
+            anh1 = itemView.findViewById(R.id.anh1);
+            btnThemvaoGiohang = itemView.findViewById(R.id.btnThemvaoGiohang);
+            btnMuaNgay = itemView.findViewById(R.id.btnMuaNgay);
+
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/du_an1_qldt/model/phone.java
+++ app/src/main/java/com/example/du_an1_qldt/model/phone.java
@@ -7,82 +7,82 @@ public class phone {
     private  int image;
     private int rom;
 
-    public void setGia(int gia) {
-        this.gia = gia;
-    }
-
     private int soLuong;
-
-    public int getGia() {
-        return gia;
-    }
 
     private int gia;
     private int id_Hang;
     private int status;
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setImage(int image) {
-        this.image = image;
-    }
-
-    public void setRom(int rom) {
-        this.rom = rom;
-    }
-
-    public void setSoLuong(int soLuong) {
-        this.soLuong = soLuong;
-    }
-
-    public void setId_Hang(int id_Hang) {
-        this.id_Hang = id_Hang;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getColor() {
         return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public int getImage() {
         return image;
     }
 
+    public void setImage(int image) {
+        this.image = image;
+    }
+
     public int getRom() {
         return rom;
+    }
+
+    public void setRom(int rom) {
+        this.rom = rom;
     }
 
     public int getSoLuong() {
         return soLuong;
     }
 
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public int getGia() {
+        return gia;
+    }
+
+    public void setGia(int gia) {
+        this.gia = gia;
+    }
+
     public int getId_Hang() {
         return id_Hang;
     }
 
+    public void setId_Hang(int id_Hang) {
+        this.id_Hang = id_Hang;
+    }
+
     public int getStatus() {
         return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public phone() {
