c938156df303506918a2fcbce84cad122acf6eaf
hainx.dev.it <hainx.dev@gmail.com>
2024-03-29 10:26:55 +0700

    create activity_tao_don_hang.xml

--- app/src/main/java/com/example/du_an1_qldt/Adapter/DSSP_Adapter.java
+++ app/src/main/java/com/example/du_an1_qldt/Adapter/DSSP_Adapter.java
@@ -2,6 +2,7 @@ package com.example.du_an1_qldt.Adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.du_an1_qldt.DAO.SanPhamDAO;
 import com.example.du_an1_qldt.R;
+import com.example.du_an1_qldt.TaoDonHang;
 import com.example.du_an1_qldt.TrangChuNguoiDung;
 import com.example.du_an1_qldt.model.phone;
 
@@ -50,7 +52,13 @@ public class DSSP_Adapter extends RecyclerView.Adapter< DSSP_Adapter.ViewHolder_
         holder.tv_tenSP1.setText( String.valueOf(dt.getName()) );
         holder.tv_giaSPham.setText( String.valueOf( dt.getGia()));
         holder.tv_TrangthaiSP1.setText( String.valueOf(dt.getStatus() ));
-
+holder.btnMuaNgay.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(context, TaoDonHang.class);
+        context.startActivity(intent);
+    }
+});
 
         phone phoneDTO = listSP.get(position);
 
